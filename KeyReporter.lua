b7aa362d50b35053f7a5dfc0c898346c1e1b67a7
EvanCobb <evan.cobb@protonmail.com>
2021-11-30 18:29:49 -0600

    adds active covenant to the end of your key report

--- KeyReporter.lua
+++ KeyReporter.lua
@@ -3,16 +3,16 @@ local L = namespace.L
 
 -- incoming chat event name -> channel to respond in
 local eventToChannel = {
+    -- uncomment for testing
+    -- CHAT_MSG_SAY = "EMOTE", -- This addon cannot send to SAY
+
     CHAT_MSG_GUILD = "GUILD",
     CHAT_MSG_PARTY = "PARTY",
     CHAT_MSG_PARTY_LEADER = "PARTY",
     CHAT_MSG_RAID = "RAID",
     CHAT_MSG_RAID_LEADER = "RAID",
     CHAT_MSG_INSTANCE_CHAT = "INSTANCE_CHAT",
-    CHAT_MSG_INSTANCE_CHAT_LEADER = "INSTANCE_CHAT",
-
-    -- uncomment for testing
-    -- CHAT_MSG_SAY = "EMOTE", -- This addon cannot send to SAY
+    CHAT_MSG_INSTANCE_CHAT_LEADER = "INSTANCE_CHAT"
 }
 
 local function BuildMessage()
@@ -22,15 +22,24 @@ local function BuildMessage()
 
     local mapID = C_MythicPlus.GetOwnedKeystoneChallengeMapID()
 
+    local covenantSuffix = ""
+    local covenantID = C_Covenants.GetActiveCovenantID()
+    if covenantID ~= 0 then
+        local covenantName = C_Covenants.GetCovenantData(covenantID)['name']
+        covenantSuffix = " (" .. covenantName .. ")"
+    end
+
     if not mapID then 
         return L["I have no key."]
     end
 
-    return "" .. C_ChallengeMode.GetMapUIInfo(mapID) .. " " .. C_MythicPlus.GetOwnedKeystoneLevel()
+    return "" .. C_ChallengeMode.GetMapUIInfo(mapID) .. " " .. C_MythicPlus.GetOwnedKeystoneLevel() .. covenantSuffix
 end
 
 local function LookForChatCommand(self, event, text)
-    if text ~= "!keys" then return end
+    if text ~= "!keys" then
+        return
+    end
     SendChatMessage(BuildMessage(), eventToChannel[event])
 end
 
